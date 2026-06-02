805cb04dbb9457008754f99ebef72970103c5cdd
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2026-06-02 14:14:04 +0330

    Filter cars by price

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,11 @@
 import carsData from "../../data/carsData"
 import Categories from "../../module/Categories"
+import SearchBar from "../../module/SearchBar"
 import CarsPage from "../../templates/CarsPage"
 function AllCars() {
     return (
         <div>
+            <SearchBar />
             <Categories />
             <CarsPage data={carsData} />
         </div>
---
+++ pages/filter/[...slug].js
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router"
+import carsData from "../../data/carsData"
+import CarsList from "../../templates/CarsList"
+function FilteredCars() {
+    const router = useRouter()
+    const [min, max] = router.query.slug || [];
+
+    const filteredData = carsData.filter(item => item.price > min && item.price < max)
+    if (!filteredData.length) return <h3>NotFound</h3>
+    return (
+        <CarsList data={filteredData} />
+    )
+}
+
+export default FilteredCars
